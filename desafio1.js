061e729e8148b716c6e98f7fb5fe73182ede0bd4
Belen.N <mariabn.078@gmail.com>
2023-10-10 00:05:06 -0300

    first commit

---
+++ desafio1.js
@@ -0,0 +1,71 @@
+class ProductManager {
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(title, description, price, code, stock) {
+        const product = {
+            id: this.#getId() + 1,
+            title,
+            description,
+            price,
+            code,
+            stock,
+            paquets: []
+        };
+        this.products.push(product);
+    }
+
+    #getId(){
+        let maxId = 0;
+        this.products.map((product)=>{
+            if(product.id > maxId) maxId = product.id;
+        })
+        return maxId;
+    };
+
+    getProducts(){
+        return this.products;
+    }
+
+    getProduct(idProduct){
+        return this.products.find((product) => product.id === idProduct);
+    }
+
+    getProductById(idProduct){
+        const product = this.getProduct(idProduct);
+        if(product) {
+            if(!product.paquets.includes(idProduct)) product.paquets.push(idProduct);
+        } else return 'Not found'
+    }
+
+    addCode(idProduct, idCode){
+        const product = this.getProduct(idProduct);
+        if(product) {
+            if(!product.paquets.includes(idCode)) product.paquets.push(idCode);
+        } else return 'this code not exists'
+    }
+
+    productNew(idProduct, newTitle, newDescription,newPrice, newCode, newStock){
+        const product = this.getProduct(idProduct);
+        if(product){
+            const newProduct = {
+                ...product,
+                id: this.#getId() + 1,
+                title: newTitle,
+                description: newDescription,
+                price: newPrice,
+                code: newCode,
+                stock: newStock,
+                products: []
+            };
+            this.products.push(newProduct);
+        } else return 'this product not exists'
+    };
+};  
+
+const productManager = new ProductManager();
+
+productManager.addProduct('Cafe', 'Cafe negro importado de Colombia', 2500, 1243, 68 );
+productManager.productNew(1, 'Pan', 'Pan integral casero', 1700, 1244, 70)
+console.log(productManager.getProducts());
