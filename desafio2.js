946cb2aa712f826aad3d9c83eb4abe83fea64753
Belen.N <mariabn.078@gmail.com>
2023-10-24 14:13:01 -0300

    modificaciones

--- desafio2.js
+++ desafio2.js
@@ -1,31 +1,33 @@
-const fs = require('fs');
+import fs from 'fs'
 
 class ProductManager {
-    constructor(){
-        this.path = './products.json'
-    } 
+    constructor() {
+        this.path = './products.json';
+    }
 
-    async getProducts(){
-        try {
-            if(fs.existsSync(this.path)) {
-               const productsJSON = await fs.promises.readFile(this.path, 'utf-8');
-               return JSON.parse(productsJSON);
-            } else return [];
-        } catch (error) {
-            console.log(error);
+    async read() {
+        if (fs.existsSync(this.path)) {
+            const productsJSON = await fs.promises.readFile(this.path, 'utf-8');
+            return JSON.parse(productsJSON);
         }
-    };
+        return [];
+    }
 
-    getNewID = list =>{
+    async write(list) {
+        await fs.promises.writeFile(this.path, JSON.stringify(list), 'utf-8');
+    }
+
+    getNewID(list) {
         const count = list.length;
-        return (count > 0) ? list[count - 1].id + 1 : 1;
-    } 
+        return count > 0 ? list[count - 1].id + 1 : 1;
+    }
 
-    addProduct = async (title, description, price, code, stock) => {
+    async addProduct(title, description, price, code, stock) {
         const list = await this.read();
         const newID = this.getNewID(list);
-        const exis = this.existProduct(code, list);
-        if (! exis < 0) {
+        const exists = this.existProduct(code, list);
+
+        if (!exists) {
             const newProduct = {
                 id: newID,
                 title,
@@ -34,92 +36,84 @@ class ProductManager {
                 code,
                 stock,
             };
+
             list.push(newProduct);
             await this.write(list);
             return newProduct;
+        } else {
+            return 'Producto existente';
         }
-        return 'producto existente';
     }
-};
+
+    existProduct(code, list) {
+        return list.some((el) => el.code === code);
+    }
+
+    async getProducts() {
+        const list = await this.read();
+        return list;
+    }
+
+    async getProductById(id) {
+        const list = await this.getProducts();
+        const product = list.find((prod) => prod.id == id);
+        return product || "El Producto no existe";
+    }
+
+    async updateProduct(id, campo, update) {
+        const list = await this.getProducts();
+        const idx = list.findIndex((e) => e.id == id);
+
+        if (idx < 0) return "El Producto no existe";
+        list[idx][campo] = update;
+        await this.write(list);
+        return list[idx][campo];
+    }
+
+    async updateProductObj(id, obj) {
+        obj.id = id;
+        const list = await this.read();
+        const idx = list.findIndex((e) => e.id == id);
+
+        if (idx < 0) return;
+        list[idx] = obj;
+        await this.write(list);
+    }
+
+    async deleteProduct(id) {
+        const list = await this.getProducts();
+        const idx = list.findIndex((e) => e.id == id);
+
+        if (idx < 0) return;
+        list.splice(idx, 1);
+        await this.write(list);
+        return list;
+    }
+}
 
 const productManager = new ProductManager();
 const product1 = {
-    id:1,
+    id: 1,
     title: 'Caffe',
     description: 'Importado',
-    price:2000,
-    code:6789,
-    stock:30,
+    price: 2000,
+    code: 6789,
+    stock: 30,
 };
 
 const product2 = {
-    id:2,
+    id: 2,
     title: 'Azucar',
     description: 'Calidad alta',
-    price:800,
-    code:6790,
-    stock:23,
+    price: 800,
+    code: 6790,
+    stock: 23,
 };
 
-read = () => {
-    if (fs.existsSync(this.path)) {
-        return fs.promises.readFile(this.path, this.format).then(r => JSON.parse(r));
-    }
-    return [];
-}
-
-getProducts = async () => {
-    const list = await this.read();
-    return list;
-}
-
-write = async list => {
-    fs.promises.writeFile(this.path, JSON.stringify(list));
-}
-
-existProduct = (code, list) => {
-    return list.some(el => el.code === code);
-}
-
-getProductbyId = async (id) => {
-    const list = await this.getProducts();
-    return list.find((prod) => prod.id == id) ?? "El Producto no existe";
-}
-
-updateProduct = async (id, campo, update) => {
-    const list = await this.getProducts();
-    const idx = list.indexOf(e => e.id == id);
-    
-    if(idx < 0) return "El Producto no existe";
-    list[idx][campo] = update;
-    await this.write(list);
-    return list[idx][campo];
-}
-
-updateProductObj = async (id, obj) => {
-    obj.id = id;
-    const list = await this.read();
-
-    const idx = list.findIndex((e) => e.id == id);
-    if (idx < 0) return;
-    list[idx] = obj;
-    await this.write(list);
-}
-
-deleteProduct = async (id) => {
-    const list = await this.getProducts();
-    const idx = list.findIndex((e) => e.id == id);
-    if (idx < 0) return;
-    list.splice(idx, 1);
-    await this.write(list);
-    return list;
-}
-
-
-const test = async() =>{
-    console.log('primer consulta', await productManager.getProducts());
+const test = async () => {
+    console.log('Primer consulta', await productManager.getProducts());
     await productManager.addProduct(product1);
-    console.log('segunda consulta', await productManager.getProducts());
+    console.log('Segunda consulta', await productManager.getProducts());
     await productManager.addProduct(product2);
 };
 
